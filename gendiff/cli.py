d9e16d374df99ed8bc3f25ca66b397bf3da09604
Mikhail Kokuev <kokmickey@yandex.ru>
2026-03-22 23:20:28 +0500

    add file reading

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,14 +1,21 @@
 import argparse
+from pathlib import Path
 
 
-def main() -> tuple[str, str, str]:
+class FileNotFoundError(Exception):
+    def __init__(self, file):
+        message = f'File {file} does not exist'
+        super().__init__(message)
+
+
+def main() -> tuple[Path, Path, str]:
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.',
         add_help=True,
     )
 
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=Path)
+    parser.add_argument('second_file', type=Path)
     parser.add_argument(
         '-f',
         '--format',
@@ -18,4 +25,12 @@ def main() -> tuple[str, str, str]:
 
     args = parser.parse_args()
 
-    return args.first_file, args.second_file, args.format
+    file1, file2, format_ = args.first_file, args.second_file, args.format
+    file1 = file1.resolve()
+    file2 = file2.resolve()
+
+    for file in (file1, file2):
+        if not file.exists():
+            raise FileNotFoundError(file)
+
+    return file1, file2, format_
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,7 +2,12 @@ from gendiff import cli
 
 
 def main():
-    print(cli.main())
+    file1, file2, format_ = cli.main()
+
+    for file in (file1, file2):
+        with open(file, 'r', encoding='utf-8') as f:
+            content = f.read()
+            print(content)
 
 
 if __name__ == '__main__':
